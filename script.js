d2c86e145a21a702b60326b9bec6133f5c0d10b7
Ahmed Hallaq <88354299+A7medhq@users.noreply.github.com>
2021-11-26 02:31:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,117 @@
-let body = document.body
+// create img for every size
+let width = "1440";
+let height = "2960";
 
-console.log(window.screen.width)
-
-if(window.screen.width > 1024) {
-    body.style.background = `url(https://picsum.photos/1920/1080)  no-repeat center center fixed`
-}
-else if (window.screen.width > 768) {
-    body.style.background = `url(https://picsum.photos/2048/1536)  no-repeat center center fixed`
-}else {
-    body.style.background = `url(https://picsum.photos/1440/2960)  no-repeat center center fixed`
-}
-
-body.style.backgroundSize = 'cover'
-
-function generateRanodmNum(max){
-    return Math.floor(Math.random() * max);
+if (window.screen.width >= 1024) {
+  width = "1920";
+  height = "1080";
+} else if (window.screen.width >= 768) {
+  width = "2048";
+  height = "1536";
+} else {
+  width = "1440";
+  height = "2960";
 }
 
-fetch('https://type.fit/api/quotes')
-.then((response)=>response.json())
-.then((res)=>{
+var img = new Image();
+img.src = `https://picsum.photos/${width}/${height}`;
 
-    let quote = document.getElementById('quote')
-    quote.textContent = res[generateRanodmNum(res.length - 1)].text
+// create canvas with the image as a background and add the caption from using the API
+var canvas = document.getElementById("canvas");
+canvas.setAttribute("width", width);
+canvas.setAttribute("height", height);
+var ctx = canvas.getContext("2d");
+img.onload = drawImageScaled.bind(null, img, ctx);
 
+function generateRanodmNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let txt = "";
+let lines = [];
+
+fetch("https://type.fit/api/quotes")
+  .then((response) => response.json())
+  .then((res) => {
+    txt = res[generateRanodmNum(res.length - 1)].text;
+    lines = txt.match(/\b[\w']+(?:[^\w\n]+[\w']+){0,2}\b/g);
+  })
+  .catch((err) => err);
+
+function drawImageScaled(img, ctx) {
+  canvas = ctx.canvas;
+
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.min(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.shadowColor = "black";
+  ctx.shadowBlur = 17;
+  var lineheight = 50;
+  ctx.font = "170px Fearlessly";
+  ctx.lineWidth = 7;
+  let offset = 750
+
+
+
+  if (window.screen.width >= 1024) {
+    ctx.font = "60px Fearlessly";
+   lineheight = 60;
+   ctx.shadowBlur = 5;
+    ctx.lineWidth = 7;
+    offset = 250
+  } 
+ else if (window.screen.width >= 768) {
+    ctx.font = "100px Fearlessly";
+    lineheight = 100;
+    ctx.shadowBlur = 5;
+     ctx.lineWidth = 7;
+     offset = 350
+  } else {
+  ctx.font = "170px Fearlessly";
+   lineheight = 150;
+  ctx.lineWidth = 7;
+  offset = 750
+  }
+
+
+  for (i = 0; i < lines.length; i++) {
+    ctx.strokeText(
+      lines[i],
+      canvas.width / 2,
+      canvas.height / 2 + i * lineheight - offset
+    );
+    ctx.fillText(
+      lines[i],
+      canvas.width / 2,
+      canvas.height / 2 + i * lineheight - offset
+    );
+  }
+}
+
+
+window.addEventListener('load', function () {
+  let imgConv = document.createElement('img')
+  imgConv.crossOrigin = 'Anonymous'
+imgConv.style.display = 'none'
+document.body.appendChild(imgConv)
+  let canvas = this.document.getElementById('canvas')
+  const dataURI = canvas.toDataURL('image/jpeg')
+  imgConv.src = dataURI
 })
-.catch((err)=>err)
-
